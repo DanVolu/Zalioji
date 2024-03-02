6722c22614063888808ec08c3b424128314733f8
DanVolu <dan.voluj@gmail.com>
2024-03-02 15:17:39 +0200

    category basics added

--- react-test/zaliasis/src/components/Header.jsx
+++ react-test/zaliasis/src/components/Header.jsx
@@ -1,16 +1,18 @@
 import '../styles/Header.css';
+// import lokysImage from '../images/lokys-transformed.jpeg';
 
 function Header() {
   return (
     <>
-    <div className='HeaderTextContainer'>
+      <div className='HeaderTextContainer'>
         <div className='HeaderTexts'>
-          <h1>Žalioji knyga</h1>
-          <p>Kažka čia kieto ir aktualaus apie tinklapį parašysiu</p>
+          {/* <img className='Lokys' src={lokysImage} alt="Lokys" /> */}
+            <h1>Žalioji knyga</h1>
+            <p>Kažka čia kieto ir aktualaus apie tinklapį parašysiu</p>
         </div>
-    </div>
+      </div>
     </>
   )
 }
 
-export default Header
+export default Header;
