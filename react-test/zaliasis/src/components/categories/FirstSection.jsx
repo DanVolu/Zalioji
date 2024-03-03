7ae45f7f9ba81b2fb4514ea17baa74ffaf7c2620
DanVolu <dan.voluj@gmail.com>
2024-03-03 10:34:12 +0200

    first section adjustments

--- react-test/zaliasis/src/components/categories/FirstSection.jsx
+++ react-test/zaliasis/src/components/categories/FirstSection.jsx
@@ -15,9 +15,11 @@ function FirstSection() {
 
         <div className='SecondFirstSectionCategory'>
             <img className='LynxImage' src={LynxImage} alt="Lūšis" />
-              <h2>Dangaus stebėtojai</h2>
-              <h3>Praplėsk žinias</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc mi ipsum faucibus vitae aliquet.</p>
+              <div className='DescriptionContainer'>  
+                <h2>Dangaus stebėtojai</h2>
+                <h3>Praplėsk žinias</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc mi ipsum faucibus vitae aliquet.</p>
+              </div>
         </div>
 
 
