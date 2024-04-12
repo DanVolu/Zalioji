9e18ccb662c2fa1e5dd3391eb1676b3ef68273e4
DanVolu <dan.voluj@gmail.com>
2024-04-12 18:53:27 +0300

    minor animal list changes

--- react-test/zaliasis/src/pages/AllAnimals.jsx
+++ react-test/zaliasis/src/pages/AllAnimals.jsx
@@ -1,14 +1,52 @@
 import NavigationBar from '../components/NavigationBar.jsx'
 import '../styles/Navbar.css'
-import AnimalList from '../components/AnimalList.jsx';
-
-
+import { Container, Row, Col } from 'react-bootstrap';
+import Lusis from '../components/animals/Lusis.jsx';
+import Pavyzdys from '../components/animals/Pavyzdys.jsx';
 
 function About() {
   return (
     <>
         <NavigationBar></NavigationBar>
-        <AnimalList></AnimalList>
+
+        
+            <Container className='animalBook' style={{marginTop: "5rem"}}>
+              <div className='animalBookList'>
+                <Row>
+                    <Col lg={6} className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+
+                <Row>
+                    <Col lg={6}  className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+
+                <Row>
+                    <Col lg={6} className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+
+                <Row>
+                    <Col lg={6}  className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+
+
+                <Row>
+                    <Col lg={6} className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+
+                <Row>
+                    <Col lg={6}  className="bookItem"><Lusis /></Col>
+                    <Col lg={6}  className="bookItem"><Pavyzdys /></Col>
+                </Row>
+              </div>
+            </Container>
+          
+          
+
     </>
   );
 }
